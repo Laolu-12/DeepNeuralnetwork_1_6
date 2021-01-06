02274f560937be6df99d735e19053e8e9e033bfe
Laolu-12 <1187891142@qq.com>
2021-01-06 10:47:50 +0800

    1

---
+++ 神经网络1_1_6/parameters.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"C:\\Users\\Lenovo\\Downloads\\Video\\奔跑视频.mp4"		
+#define OPENPOSE_VIDEO		"C:\\Users\\Lenovo\\Videos\\日用\\openposeTest.mp4"		
+
+//#define INPUT_METHOD	
